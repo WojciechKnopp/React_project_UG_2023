f72acfb9a387ad9713d9ad25753417f90b3c4739
Wojtek <umboj2000@gmail.com>
2023-06-06 22:51:55 +0200

    aktualizacja i naprawa strony szczegółów produktu

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { useProductContext } from '../context/ProductContext';
+// import { useProductContext } from '../context/ProductContext';
 import PopupTimer from './PopupTimer';
+import axios from 'axios';
 
 const ProductDetails = () => {
     const { id } = useParams();
-    const { products } = useProductContext();
-    const product = products.find(product => product.id === parseInt(id));
+    // const { products } = useProductContext();
+    // const product = products.find(product => product.id === parseInt(id));
+
+    const [product, setProduct] = useState([]);
+    useEffect(() => {
+        const fetchProduct = async () => {
+            try{
+                const response = await axios.get(`http://127.0.0.1:8000/api/products/${id}/`);
+                setProduct(response.data);
+            } catch(err){
+                console.log(err);
+            }
+        };
+
+        fetchProduct();
+    }, [id]);
 
     return (
-        <div className="product details" key={product.id}>
+        <div className="product details pb-3" key={product.id}>
             <img className='card-img-top' src={product.image} alt={product.name} />
             <div className="card-body">
                 <h3 className="card-title">{product.name}</h3>
                 <p className="card-text"><strong>{product.price} zł</strong></p>
-                <p>{product.description}</p>
+                <p><strong>Opis: </strong><br/>{product.description}</p>
+                <p><strong>Składniki: </strong>{product.ingredients}</p>
             </div>
             <Link to='/products'><button className='ovr-btn'>Wróć do produktów</button></Link>
             <PopupTimer />
